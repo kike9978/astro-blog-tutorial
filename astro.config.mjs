3df65f538bbce2652c0d7017e2645d8c72b74f80
Kike Mendoza <kike.mendoza9978@gmail.com>
2023-12-19 20:25:21 -0600

    Create dark mode toggle button

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://k-astro-blog-tutorial.netlify.app/"
+  site: "https://k-astro-blog-tutorial.netlify.app/",
+  integrations: [preact()]
 });
